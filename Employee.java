f74682ae2a510bbb6db7e933f28f1e3fff640918
saadhvi <01fe22bca104@kletech.ac.in>
2024-03-31 14:59:04 +0530

    code snippet to display employee's info

---
+++ Employee.java
@@ -0,0 +1,13 @@
+public class Employee{
+    public int empid=104;
+    public String name="Saadhvi";
+    public float salary=7000000;
+    public void display_details()
+    {
+        System.out.println("Employee Details:");
+        System.out.println("Employee Id"+empid);
+        System.out.println("Employee Name"+name);
+        System.out.println("Salary"+salary);
+    
+    }
+}
